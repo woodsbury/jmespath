f46faad39d48ec1560be384aa3b058410042a204
Wade Smith <wade.cameron@gmail.com>
2024-11-15 20:00:17 +1000

    fix error return values
    Fix the error return values from Expression objects to be consistent
    with the top-level Search function.

--- jmespath.go
+++ jmespath.go
@@ -12,48 +12,12 @@ import (
 func Search(expression string, data any) (any, error) {
 	node, err := parser.Parse(expression)
 	if err != nil {
-		if err, ok := err.(*parser.InvalidFunctionArgumentError); ok {
-			return nil, &invalidTypeError{err.Error()}
-		}
-
-		if err, ok := err.(*parser.InvalidFunctionCallError); ok {
-			return nil, &invalidFunctionCallError{err.Function}
-		}
-
-		if _, ok := err.(*parser.InvalidSliceStepError); ok {
-			return nil, &invalidSliceStepError{}
-		}
-
-		if err, ok := err.(*parser.UnknownFunctionError); ok {
-			return nil, &unknownFunctionError{err.Function}
-		}
-
-		return nil, &invalidExpressionError{expression, err.Error()}
+		return nil, parseError(expression, err)
 	}
 
 	result, err := evaluator.Evaluate(node, data)
 	if err != nil {
-		if errors.Is(err, evaluator.ErrInvalidType) {
-			return nil, &invalidTypeError{err.Error()}
-		}
-
-		if errors.Is(err, evaluator.ErrInvalidValue) {
-			return nil, &invalidValueError{err.Error()}
-		}
-
-		if errors.Is(err, evaluator.ErrInfinity) {
-			return nil, &infinityError{}
-		}
-
-		if errors.Is(err, evaluator.ErrNotANumber) {
-			return nil, &notANumberError{}
-		}
-
-		if err, ok := err.(*evaluator.UndefinedVariableError); ok {
-			return nil, &undefinedVariableError{err.Variable}
-		}
-
-		return nil, &evaluationFailedError{err.Error()}
+		return nil, evaluateError(err)
 	}
 
 	return result, nil
@@ -69,7 +33,7 @@ type Expression struct {
 func Compile(expression string) (*Expression, error) {
 	node, err := parser.Parse(expression)
 	if err != nil {
-		return nil, &invalidExpressionError{expression, err.Error()}
+		return nil, parseError(expression, err)
 	}
 
 	return &Expression{
@@ -92,15 +56,55 @@ func MustCompile(expression string) *Expression {
 
 // Search evaluates the compiled expression against data and returns the
 // result.
-func (e Expression) Search(data any) (any, error) {
+func (e *Expression) Search(data any) (any, error) {
 	result, err := evaluator.Evaluate(e.node, data)
 	if err != nil {
-		if errors.Is(err, evaluator.ErrNotANumber) {
-			return nil, &notANumberError{}
-		}
-
-		return nil, &evaluationFailedError{err.Error()}
+		return nil, evaluateError(err)
 	}
 
 	return result, nil
+}
+
+func evaluateError(err error) error {
+	if errors.Is(err, evaluator.ErrInvalidType) {
+		return &invalidTypeError{err.Error()}
+	}
+
+	if errors.Is(err, evaluator.ErrInvalidValue) {
+		return &invalidValueError{err.Error()}
+	}
+
+	if errors.Is(err, evaluator.ErrInfinity) {
+		return &infinityError{}
+	}
+
+	if errors.Is(err, evaluator.ErrNotANumber) {
+		return &notANumberError{}
+	}
+
+	if err, ok := err.(*evaluator.UndefinedVariableError); ok {
+		return &undefinedVariableError{err.Variable}
+	}
+
+	return &evaluationFailedError{err.Error()}
+}
+
+func parseError(expression string, err error) error {
+	if err, ok := err.(*parser.InvalidFunctionArgumentError); ok {
+		return &invalidTypeError{err.Error()}
+	}
+
+	if err, ok := err.(*parser.InvalidFunctionCallError); ok {
+		return &invalidFunctionCallError{err.Function}
+	}
+
+	if _, ok := err.(*parser.InvalidSliceStepError); ok {
+		return &invalidSliceStepError{}
+	}
+
+	if err, ok := err.(*parser.UnknownFunctionError); ok {
+		return &unknownFunctionError{err.Function}
+	}
+
+	return &invalidExpressionError{expression, err.Error()}
 }
